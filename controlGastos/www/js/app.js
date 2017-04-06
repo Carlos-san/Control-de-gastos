21b0682a2ea4e9fdaa20b4b883a1719e20bbd838
Clare <clarero1611@gmail.com>
2017-04-06 15:51:11 -0500

    Agrega plugin de mascaras de texto, fechas y horas, modal.

--- controlGastos/www/js/app.js
+++ controlGastos/www/js/app.js
@@ -4,8 +4,10 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var controlGastosModule = angular.module('starter', ['ionic', 'starter.controllers', 'ion-floating-menu']);
-var controladoresModule = angular.module('starter.controllers', ['starter.services']);
+var controlGastosModule = angular.module('starter',
+  ['ionic', 'starter.controllers', 'ion-floating-menu',
+   'ui.utils.masks', 'ion-datetime-picker','angularMoment']);
+var controladoresModule = angular.module('starter.controllers', []);
 var serviciosModule = angular.module('starter.services', ['ngCordova']);
 
 controlGastosModule.run(function($ionicPlatform) {
--- controlGastos/www/js/controller/app.controller.js
+++ controlGastos/www/js/controller/app.controller.js
@@ -1,3 +1,39 @@
 controladoresModule.controller('AppCtrl',AppCtrl);
-function AppCtrl ($scope){}
-AppCtrl.$inject = ['$scope'];
+function AppCtrl ($scope,$ionicModal){
+
+
+  /*----------Registro de monto base----------*/
+  $scope.montoBase = {
+    valor: 0,
+    periodoInicial: new Date(),
+    periodoFinal: new Date()
+  }
+
+  $scope.bntAgregarBase = function () {
+    $ionicModal.fromTemplateUrl('templates/modal/agregar-base.html', {
+      scope: $scope,
+      animation: 'fade-in-scale'
+    }).then(function(modal) {
+      $scope.modalBase = modal;
+      $scope.modalBase.show();
+    });
+  }
+  $scope.agregarBase = function(){
+    console.log(moment($scope.montoBase.valor).format("DD/MM/YYYY"));
+    console.log(moment($scope.montoBase.periodoInicial).format("DD/MM/YYYY"));
+    console.log(moment($scope.montoBase.periodoFinal).format("DD/MM/YYYY"));
+  }
+  $scope.cerrarBase = function() {$scope.modalBase.hide();};
+  /*----------------------------------------*/
+
+  $scope.agregarSalida = function () {
+    alert("Agrega una salida");
+  }
+  $scope.agregarEntrada = function () {
+    alert("Agrega una entrada");
+  }
+  $scope.crearActividad = function () {
+    alert("Agrega una actividad");
+  }
+}
+AppCtrl.$inject = ['$scope','$ionicModal'];
