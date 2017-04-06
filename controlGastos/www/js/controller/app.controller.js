d45b75e23366e16fbb2033888ded83db49bd2873
Clare <clarero1611@gmail.com>
2017-04-06 17:31:36 -0500

    Agrega funcionalidad de agregar salidas

--- controlGastos/www/js/controller/app.controller.js
+++ controlGastos/www/js/controller/app.controller.js
@@ -9,6 +9,14 @@ function AppCtrl ($scope,$ionicModal){
     }
   }
 
+  function inicializaSalida(){
+    $scope.montoSalida = {
+      valor: 0,
+      valorMax:0,
+      detalle:"",
+      fecha: new Date()
+    }
+  }
   /*----------Registro de monto base----------*/
 
   $scope.bntAgregarBase = function () {
@@ -22,16 +30,37 @@ function AppCtrl ($scope,$ionicModal){
     });
   }
   $scope.agregarBase = function(){
-    console.log(moment($scope.montoBase.valor).format("DD/MM/YYYY"));
+    console.log($scope.montoBase.valor);
     console.log(moment($scope.montoBase.periodoInicial).format("DD/MM/YYYY"));
     console.log(moment($scope.montoBase.periodoFinal).format("DD/MM/YYYY"));
   }
   $scope.cerrarBase = function() {$scope.modalBase.hide();};
   /*----------------------------------------*/
 
-  $scope.agregarSalida = function () {
-    alert("Agrega una salida");
+  /*----------Registro de monto salida----------*/
+
+  $scope.bntAgregarSalida = function () {
+    inicializaSalida();
+    // Monto actual
+
+    $scope.montoSalida.valorMax = 10000;
+    console.log($scope.montoSalida);
+    $ionicModal.fromTemplateUrl('templates/modal/agregar-salida.html', {
+      scope: $scope,
+      animation: 'fade-in-scale'
+    }).then(function(modal) {
+      $scope.modalSalida = modal;
+      $scope.modalSalida.show();
+    });
   }
+  $scope.agregarSalida = function () {
+    console.log($scope.montoSalida.valor);
+    console.log($scope.montoSalida.valorMax);
+    console.log($scope.montoSalida.fecha);
+    console.log($scope.montoSalida.detalle);
+  }
+  $scope.cerrarSalida = function() {$scope.modalSalida.hide();};
+  /*----------------------------------------*/
   $scope.agregarEntrada = function () {
     alert("Agrega una entrada");
   }
@@ -39,5 +68,6 @@ function AppCtrl ($scope,$ionicModal){
     alert("Agrega una actividad");
   }
   inicializaMontoBase();
+  inicializaSalida();
 }
 AppCtrl.$inject = ['$scope','$ionicModal'];
