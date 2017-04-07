9c7939bbcfe1392fd2bdf5de5c3698774664557e
CarlosMontoya <cm.090691@gmail.com>
2017-04-06 21:15:33 -0500

    menor: Quita debugger

--- controlGastos/www/js/controller/test.controller.js
+++ controlGastos/www/js/controller/test.controller.js
@@ -3,9 +3,7 @@ controladoresModule.controller('TestCtrl',TestCtrl);
 function TestCtrl($scope, movimientosService){
 
   $scope.$on('creacionBase', function(){
-    debugger;
     movimientosService.obtenerBaseActual().then(function(data){
-      debugger;
       var base = data;
     }, function(err){
       alert("paila");
