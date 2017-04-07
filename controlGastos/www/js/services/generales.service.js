789eac31f06728ebd36cba3cdd4b825a72288914
CarlosMontoya <cm.090691@gmail.com>
2017-04-07 14:02:12 -0500

    Integración de movimientos en BD y con registro de base

---
+++ controlGastos/www/js/services/generales.service.js
@@ -0,0 +1,20 @@
+serviciosModule.service("generalesService", generalesService);
+
+function generalesService($cordovaToast){
+
+  //Muestra un mensaje por defecto
+  var generarMensajeCorto = function( mensaje){
+    if(window.cordova)
+      $cordovaToast.show(mensaje, 'short', 'center')
+        .then(function(success) {}, function (error) {});
+    else{
+      alert(mensaje);
+    }
+  }
+
+  return {
+    generarMensajeCorto : generarMensajeCorto
+  }
+}
+
+generalesService.$inject = ['$cordovaToast']
