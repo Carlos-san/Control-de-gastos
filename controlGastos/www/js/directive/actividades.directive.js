b0f071dce2fb50bd8581c3b1de7fd2857f9d1ffa
Clare <clarero1611@gmail.com>
2017-04-07 09:31:50 -0500

    menor: Agrega scope a partial de activdades

--- controlGastos/www/js/directive/actividades.directive.js
+++ controlGastos/www/js/directive/actividades.directive.js
@@ -2,6 +2,9 @@ controladoresModule.directive('actividadesDir',actividadesDir);
 
 function actividadesDir(){
   return{
+    scope:{
+      actividades: '=actividades'
+    },
     templateUrl:'../../templates/directive/actividades.html'
   }
 }
