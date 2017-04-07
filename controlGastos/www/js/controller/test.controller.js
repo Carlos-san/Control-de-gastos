5e957e9d81f0cdff2cbceb65995a449e0cd3e69f
Clare <clarero1611@gmail.com>
2017-04-06 23:51:12 -0500

    menor (Ajusta estilos css)

--- controlGastos/www/js/controller/test.controller.js
+++ controlGastos/www/js/controller/test.controller.js
@@ -1,7 +1,10 @@
 controladoresModule.controller('TestCtrl',TestCtrl);
 
 function TestCtrl($scope, movimientosService){
-
+ $scope.actividades = [
+   { titulo: 'Boyacá',tipo:'Vacaciones', descripcion: 'Viaje en semana santa', icon:'ion-android-plane' },
+   { titulo: 'Hoteles',tipo:'Hospedaje', descripcion: 'fectivo para hospedajes', icon:'ion-ios-home' },
+ ];
   $scope.$on('creacionBase', function(){
     movimientosService.obtenerBaseActual().then(function(data){
       var base = data;
